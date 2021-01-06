2601e56fbec05745b3c7411b7ca5a69dd3074772
Nathanael <ngreese7@ou.edu>
2021-01-06 02:12:28 -0600

    Added comments

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@
 using namespace std;
 
 int main() {
-    int randNum = rand() % 10 + 1;
-    int guess = 0;
-    while (guess != randNum) {
-        cout << "Enter your guess: " << endl;
-        cin >> guess;
-        if (guess == randNum) {
-            cout << "Congrats you guessed it!" << endl;
+    int max;
+    int min;
+    cout << "Enter Maximum Value:" << endl; //prompting maximum value from user
+    cin >> max;
+    cout << "Enter Minimum Value:" << endl; //prompting min
+    cin >> min;
+
+    int randNum = rand() % max + min; // using random function to generate number between two values of max and min
+    int guess = 0; //generating random number
+
+    while (guess != randNum) { //using while loop to keep asking to enter guess if user does not get it
+        cout << "Enter your guess: " << endl; //prompts user for guess
+        cin >> guess; //user input to guess
+        if (guess == randNum) { //if guess is correct the while loop is broken and user is congratulated
+            cout << "Congrats you guessed it!" << endl; //poggers
             break;
         }
     }
