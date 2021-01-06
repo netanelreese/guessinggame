2f104004d6b0a99a9905b515604fb71d0383551f
Nathanael <ngreese7@ou.edu>
2021-01-06 02:33:36 -0600

    Added guess limit and loss message/if statement

--- main.cpp
+++ main.cpp
@@ -15,18 +15,34 @@ int main() {
     int randNum = rand() % max + min; // using random function to generate number between two values of max and min
     int guess = 0; //generating random number
 
+    int guessLimit;
+    cout << "Enter your guess limit:" << endl;
+    cin >> guessLimit;
+
     while (guess != randNum) { //using while loop to keep asking to enter guess if user does not get it
+
+        cout << "Guess Limit: " << guessLimit << endl; //shows user the amount of guesses left
+       if (guessLimit == 0) { //if guesses equals zero the loss message is shown and while loop is broken
+            cout << "You Lose!! Dumbass!!!" << endl;
+            cout << "The answer was: " << randNum << endl;
+            break;
+        }
+
         cout << "Enter your guess: " << endl; //prompts user for guess
         cin >> guess; //user input to guess
+
         if (guess == randNum) { //if guess is correct the while loop is broken and user is congratulated
             cout << "Congrats you guessed it!" << endl; //poggers
             break;
         }
+
         else if (guess < randNum) {
             cout << "The secret number is higher..." << endl;
+            --guessLimit;
         }
         else if (guess > randNum) {
             cout << "The secret number is lower..." << endl;
+            --guessLimit;
         }
     }
 
