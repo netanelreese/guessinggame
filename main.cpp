7602ddd135024d489d261ede5300eb17bdb4d612
Nathanael <ngreese7@ou.edu>
2021-01-06 02:05:23 -0600

    Created guessing game, need to limit randomness even more

--- main.cpp
+++ main.cpp
@@ -5,8 +5,16 @@
 using namespace std;
 
 int main() {
-
-    
+    int randNum = rand();
+    int guess = 0;
+    while (guess != randNum) {
+        cout << "Enter your guess: " << endl;
+        cin >> guess;
+        if (guess == randNum) {
+            cout << "Congrats!" << endl;
+            break;
+        }
+    }
 
     return 0;
 }
